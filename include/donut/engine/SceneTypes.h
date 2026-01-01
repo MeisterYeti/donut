eaeb7eff24b1cf7dccc0245458bfad3f7d3d0034
Sascha Brandt <sascha@brandt.graphics>
2026-01-01 11:38:47 +0100

    fixed typo

--- include/donut/engine/SceneTypes.h
+++ include/donut/engine/SceneTypes.h
@@ -277,7 +277,7 @@ namespace donut::engine
         nvrhi::BufferHandle instanceBuffer;
         std::shared_ptr<DescriptorHandle> indexBufferDescriptor;
         std::shared_ptr<DescriptorHandle> vertexBufferDescriptor;
-        std::shared_ptr<DescriptorHandle> instnaceBufferDescriptor;
+        std::shared_ptr<DescriptorHandle> instanceBufferDescriptor;
         std::array<nvrhi::BufferRange, size_t(VertexAttribute::Count)> vertexBufferRanges;
         std::vector<nvrhi::BufferRange> morphTargetBufferRange;
         std::vector<uint32_t> indexData;
